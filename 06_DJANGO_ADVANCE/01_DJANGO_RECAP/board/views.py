51f1e3d2dbc56145f5d789c28baf8625370f1605
minkishome <minkishome@gmail.com>
2019-10-07 10:31:07 +0900

    finish board/index page

--- 06_DJANGO_ADVANCE/01_DJANGO_RECAP/board/views.py
+++ 06_DJANGO_ADVANCE/01_DJANGO_RECAP/board/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+
+    return render(request, 'board/index.html') # return render()는 html로 넘기겠다는 의미 뒤에 ''는 templates

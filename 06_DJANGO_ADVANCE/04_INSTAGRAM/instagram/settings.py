d83d48ab84a01407b9e49bd457a876a121c932a0
minkishome <minkishome@gmail.com>
2019-10-22 10:53:08 +0900

    set User Model @ accounts

--- 06_DJANGO_ADVANCE/04_INSTAGRAM/instagram/settings.py
+++ 06_DJANGO_ADVANCE/04_INSTAGRAM/instagram/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'postings', 
 
 ]
 
@@ -116,10 +118,16 @@ USE_L10N = True
 USE_TZ = True
 
 
+AUTH_USER_MODEL = 'accounts.User'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
 
-MEDIA_URL = '/media'
+]
+
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

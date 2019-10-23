76de8947610cd8f4310385a6117feacc2698e561
minkishome <minkishome@gmail.com>
2019-10-23 13:59:21 +0900

    |191023|

--- 06_DJANGO_ADVANCE/04_INSTAGRAM/postings/models.py
+++ 06_DJANGO_ADVANCE/04_INSTAGRAM/postings/models.py
@@ -4,37 +4,39 @@ from django_extensions.db.models import TimeStampedModel
 from django.contrib.auth import get_user_model
 
 from imagekit.models import ProcessedImageField
-from imagekit.processors import ResizeToFill
+from imagekit.processors import ResizeToFit, ResizeCanvas, Anchor
 
 User = get_user_model()
-class HashTag(TimeStampedModel):
-    content = models.CharField(max_length=20)
 
-# Create your models here.
+# class HashTag(TimeStampedModel):
+#     content = models.CharField(max_length=20, unique=True)
+
 class Posting(TimeStampedModel):
-    like_users = models.ManyToManyField(User, related_name='like_posts')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='postings')
+    # like_users = models.ManyToManyField(User, related_name='like_posts', blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='postings')
+    # hashtags = models.ManyToManyField(HashTag, blank=True, related_name='postings')
     content = models.CharField(max_length=140)
-    hastags = models.ManyToManyField(HashTag, blank=True, related_name='postings')
 
     class Meta:
-        ordering = ('-created', )
+        ordering = ('-created',)
 
     def get_absolute_url(self):
-        return reverse("posting:posting_detail", kwargs={"posting_id": self.pk})
-        
+        return reverse("postings:posting_detail", kwargs={"posting_id": self.pk})
 
 class Image(models.Model):
-    posting = models.ForeignKey(Posting, on_delete=models.CASCADE, related_name='Image')
+    posting = models.ForeignKey(Posting, on_delete=models.CASCADE, related_name='images')
+    # file = models.ImageField()
     file = ProcessedImageField(
-        processors=[ResizeToFill(600,600)],
-        upload_to='posting/images',
+        processors=[ResizeToFit(600, 600, mat_color=(45,45,45))],  # 수정
+        upload_to='postings/images',
         format='JPEG',
-        options={'quality':90},
+        options={'quality': 90},
     )
 
-class Comment(TimeStampedModel):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
-    posting = models.ForeignKey(Posting, on_delete=models.CASCADE, related_name='comments')
-    content = models.CharField(max_length=140)
+# class Comment(TimeStampedModel):
+#     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+#     posting = models.ForeignKey(Posting, on_delete=models.CASCADE, related_name='comments')
+#     content = models.CharField(max_length=140)
+
+
 
--- 06_DJANGO_ADVANCE/04_INSTAGRAM/postings/views.py
+++ 06_DJANGO_ADVANCE/04_INSTAGRAM/postings/views.py
@@ -3,7 +3,7 @@ from django.views.decorators.http import require_http_methods, require_GET, requ
 from django.contrib.auth.decorators import login_required
 
 from .forms import PostingForm, ImageForm
-from .models import Posting
+from .models import Posting, Image
 
 
 # Create your views here.
@@ -11,51 +11,47 @@ from .models import Posting
 @require_GET
 def posting_detail(request, posting_id):
     posting = get_object_or_404(Posting, id=posting_id)
-    return render(request, 'posting/posting_detail.html', {
+    return render(request, 'postings/posting_detail.html', {
         'posting':posting,
     })
 
 @require_GET
 def posting_list(request):
     postings = Posting.objects.all()
-    return render(request, 'posting/posting_list.html',{
+    return render(request, 'postings/posting_list.html',{
         'postings':postings,
     })
 
 
+
+
+@login_required
 @require_http_methods(['GET', 'POST'])
 def create_posting(request):
     images = request.FILES.getlist('file')
-    if request.method=='POST':
-        posting_form = PostingForm(request.POST)    
-        if posting_form.is_valid() and len(images) <=5:
+    if request.method == 'POST':
+        posting_form = PostingForm(request.POST)
+        if posting_form.is_valid() and len(images) <= 5: 
             posting = posting_form.save(commit=False)
             posting.author = request.user
             posting.save()
             for image in images:
-                request.FILES['file'] = image 
-                image_form = ImageForm(files=request.FILES) # Form류는 request로 시작하는 객체여야만 사용가능.
+                request.FILES['file'] = image
+                image_form = ImageForm(files=request.FILES)
                 if image_form.is_valid():
                     image = image_form.save(commit=False)
                     image.posting = posting
                     image.save()
             return redirect(posting)
-
-
-
-
-
-       
-
-            
-        
     else:
         posting_form = PostingForm()
         image_form = ImageForm()
-    return render(request, 'postings/posting_form.html',{
-        'posting_form':posting_form,
-        'image_form' : image_form,
+    return render(request, 'postings/posting_form.html', {
+        'posting_form': posting_form,
+        'image_form': image_form,
     })
+            
+
 @require_http_methods(['GET', 'POST'])
 def update_posting(request, posting_id):
     posting = get_object_or_404(Posting, id = posting_id)

035be9d22e68a9d23f67e01f6fbf2c0dddaf227c
minkishome <minkishome@gmail.com>
2020-01-29 23:49:37 +0900

    1194_problem

---
+++ 알고리즘-파이참/200129/1194(moon).py
@@ -0,0 +1,31 @@
+# def is_wall(x, y):
+#     return 0 <= x < N and 0 <= y < M
+# def move(ls, i, j, cnt):
+#     if ls[i][j] == 1:
+#         return cnt
+#     elif ls[i][j] == '#':
+#         break #벽을 만나면 깬다.
+#     elif ls[i][j] in key and not in ls_key:
+#         ls_key.append(ls[i][j]) # 내가 가지고 있지 않을때는 키 리스트에 추가
+#     elif 
+
+#     else:
+#         for k in ls_move:
+#             new_i = i + k[0]
+#             new_j = j + k[1]
+#             if is_wall(new_i, new_j):
+
+#     return
+
+
+
+# N, M = map(int, input().split()) # N= garo M = sero
+# ls_move = [(1,0), (0,1), (-1,0), (0,-1)]
+# ls_find = [list(map(int, input().split())) for _ in range(N)]
+# ls_key = []
+# key = ['a','b','c','d','e','f','g']
+# door = ['A','B','C','D','E','F','G']
+
+a = list(input().split())
+print(a)
+# list(input().split())
